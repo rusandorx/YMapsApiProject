3377cf24c076c1adceb75f1370f4906343f8e8d9
Almaz <3112almaz@gmail.com>
2023-02-20 18:24:16 +0300

    2

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import os
 import sys
 
 import requests
+from PyQt5.QtCore import Qt
 from PyQt5.QtGui import QPixmap
 from PyQt5.QtWidgets import QApplication, QWidget, QLabel
 
@@ -11,11 +12,24 @@ SCREEN_SIZE = [600, 450]
 class Example(QWidget):
     def __init__(self):
         super().__init__()
+        self.zoom = 1
+        self.image = QLabel(self)
+        self.getImage()
+        self.initUI()
+        self.setMouseTracking(True)
+
+    def keyPressEvent(self, event):
+        if event.key() == Qt.Key_PageUp:
+            if self.zoom <= 5:
+                self.zoom /= 0.5
+        if event.key() == Qt.Key_PageDown:
+            if self.zoom >= 0.02:
+                self.zoom *= 0.5
         self.getImage()
         self.initUI()
 
     def getImage(self):
-        map_request = "http://static-maps.yandex.ru/1.x/?&ll=37.617644,55.755819&spn=1,0.00619&l=map"
+        map_request = f"http://static-maps.yandex.ru/1.x/?&ll=37.617644,55.755819&spn={self.zoom},0.00619&l=map"
         response = requests.get(map_request)
 
         if not response:
@@ -31,9 +45,7 @@ class Example(QWidget):
     def initUI(self):
         self.setGeometry(100, 100, *SCREEN_SIZE)
         self.setWindowTitle('Отображение карты')
-
         self.pixmap = QPixmap(self.map_file)
-        self.image = QLabel(self)
         self.image.move(0, 0)
         self.image.resize(600, 450)
         self.image.setPixmap(self.pixmap)
